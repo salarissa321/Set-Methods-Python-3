61f690fa1417a8d8dfaae577f31dabd4909bf3c9
salarissa321 <salarissa321@gmail.com>
2023-04-26 13:33:46 +0200

    add Set-Python 2

---
+++ Set-Methods-python 3.py
@@ -0,0 +1,47 @@
+
+
+
+
+#----------------
+#----Set Methods -------
+#---------------
+
+
+# Issuperset()
+
+
+a = {1,2,3,4}
+b = {1,2,3}
+c = {1,2,3,4,5}
+
+
+print(a.issuperset(b)) # True
+print(a.issuperset(c)) # False
+
+
+print("-" * 40) # ----------------------------------------
+
+
+# issubset()
+
+d = {1,2,3,4}
+e = {1,2,3}
+f = {1,2,3,4,5}
+
+
+print{d.issubset(e)} # False 
+print{d.issubset(f)} # True 
+
+
+print("-" * 40) # ----------------------------------------
+
+
+# isdisjoint()
+
+g = {1,2,3,4}
+x = {1,2,3}
+i = {10,12,14}
+
+print(g.isdisjoint(x)) # False
+print(g.isdisjoint(i)) # True
+
